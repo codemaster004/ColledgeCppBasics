1e11c75acbb9479c38771330383f224fe846f5e6
Filip Dabkowski <filip.dabkowski@gmail.com>
2023-10-18 22:00:09 +0200

    implement list for storing trees

--- week3/treeGroth.cpp
+++ week3/treeGroth.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <string>
-#include <math.h>
+#include <cmath>
+#include <cassert>
 
 using namespace std;
 
+#define PI 3.1415
+
 const int maxNumTrees = 2;
+
+float calcHypotenuse2(float a, float b) {
+    return a * a + b * b;
+}
 
 void treeGame() {
     float **trees = new float *[maxNumTrees];
@@ -19,115 +26,98 @@ void treeGame() {
         string command;
         cin >> command;
         if (command == "ADD") {
-            int id;
+            int id, index;
             cin >> id;
+            index = id - 1;
 
-            if (trees[id] != nullptr) {
-                cout << trees[id][0];
-                if (trees[id][0]) {
+            if (trees[index] != nullptr) {
+                if (trees[index][0] == 1) {
                     cout << "Tree already active" << endl;
                     continue;
                 }
 
-                trees[id][0] = 1;
-                cin >> trees[id][1];
-                cin >> trees[id][2];
-                cin >> trees[id][3];
-                cin >> trees[id][4];
-            }
-            if (id == 2) {
-                if (t2active) {
-                    cout << "Tree already active" << endl;
-                    continue;
-                }
-                cin >> t2y;
-                cin >> t2x;
-                cin >> t2r;
-                cin >> t2h;
-                t2active = true;
+                trees[index][0] = 1; // isActive
+                cin >> trees[index][1]; // posY
+                cin >> trees[index][2]; // posX
+                cin >> trees[index][3]; // treeR
+                cin >> trees[index][4]; // treeH
             }
         } else if (command == "MDL") {
             int mode;
             cin >> mode;
             if (mode == 0) {
                 cin >> r_incr >> h_incr;
+                // TODO: Simulation
             }
         } else if (command == "PRT") {
             int mode;
             cin >> mode;
             if (mode == 3) {
+                /* Print the status of the board in the area [x1, x2] x [y1, y2] in the following way.
+                 * Divide the board into squares of size 1 x 1.
+                 * If a center of a given square is inside a living tree and the desired rectangle, print T letter.
+                 * Otherwise, print .
+                 * */
                 int x1, x2, y1, y2;
                 cin >> x1;
                 cin >> x2;
                 cin >> y1;
                 cin >> y2;
-            } else {
+                // TODO: some visualisation
+            } else if (mode == 1 || mode == 2) {
                 cout << "Trees:" << endl;
-                cout << "1 at " << t1y << ", " << t1x << " r=" << t1r << " h=" << t1h << endl;
-                if (mode == 2) {
-                    cout << " Interfering with: ";
-                    if ((t1y - t2y) * (t1y - t2y) + (t1x - t2x) * (t1x - t2x) <= (t1r + t2r) * (t1r + t2r)) {
-                        cout << "2" << endl;;
-                        float shiftx = t2x - t1x;
-                        float shifty = t2y - t1y;
-                        float r = sqrt(shiftx * shiftx + shifty * shifty);
-                        float shiftx2 = shiftx / r;
-                        float shifty2 = shifty / r;
+                for (int i = 0; i < maxNumTrees; ++i) {
+                    if (trees[i] == nullptr)
+                        continue;
 
-                        float arccos = acos(shifty2);
-
-                        float prevAngle = -22.5 / 180.0 * 3.1415;
-
-                        int sector = -1;
-                        for (int i = 0; i < 8; i++) {
-                            float angle = prevAngle + 45.0 / 180.0 * 3.1415;
-                            if (arccos >= prevAngle && arccos <= angle) {
-                                sector = i;
-                            }
-                            prevAngle = angle;
-                        }
-                        if (sector == -1) {
-                            cout << "???";
-                            return;
-                        }
-
-//                        if (sector == -1) {
-//                            cout << "ERROR" << endl;
-//                            return;
-//                        }
-                    }
-                    cout << endl;
+                    cout << (i + 1) << " at " << trees[i][1] << ", " << trees[i][2] << " r=" << trees[i][3] << " h=" << trees[i][4] << endl;
                 }
-                cout << "2 at " << t2y << ", " << t2x << " r=" << t2r << " h=" << t2h << endl;
-                if (mode == 2) {
-                    cout << " Interfering with: ";
-                    if ((t1y - t2y) * (t1y - t2y) + (t1x - t2x) * (t1x - t2x) <= (t1r + t2r) * (t1r + t2r)) {
-                        cout << "1";
-                    }
-                    cout << endl;
-                }
+//                cout << "1 at " << t1y << ", " << t1x << " r=" << t1r << " h=" << t1h << endl;
+//                if (mode == 2) {
+//                    cout << " Interfering with: ";
+//                    float shiftX = t2x - t1x;
+//                    float shiftY = t2y - t1y;
+//                    int sector = 0;
+//                    if (calcHypotenuse2(shiftX, shiftY) <= (t1r + t2r) * (t1r + t2r)) {
+//                        float r = sqrt(calcHypotenuse2(shiftX, shiftY));
+//                        float numAngle = shiftY / r;
+//
+//                        float arcCos = acos(numAngle);
+//
+//                        sector = (int)((arcCos * 8) / PI);
+//                        assert(sector >= 0 && sector <= 7);
+//                        cout << "COMPASS: " << sector << endl;
+//                    }
+//                }
+//                cout << "2 at " << t2y << ", " << t2x << " r=" << t2r << " h=" << t2h << endl;
             }
         } else if (command == "REM") {
             int id;
+            cin >> id;
             if (id == 1) {
-                t1active = false;
+//                t1active = false;
             } else if (id == 2) {
-                t2active = false;
+//                t2active = false;
             }
         } else if (command == "ADV") {
             int nEpochs;
             cin >> nEpochs;
-            t1r += nEpochs * r_incr;
-            t2r += nEpochs * r_incr;
+//            t1r += nEpochs * r_incr;
+//            t2r += nEpochs * r_incr;
 
-            t1h += nEpochs * h_incr;
-            t2h += nEpochs * h_incr;
+//            t1h += nEpochs * h_incr;
+//            t2h += nEpochs * h_incr;
         } else if (command == "END") {
             break;
         } else {
             cout << "Unknow command" << endl;
         }
     }
+}
+
+int main() {
+    treeGame();
+    return 0;
 }
 
 /*
